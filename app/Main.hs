9de38db31f4b799ee1582dba5789a0f812537eaf
EstuardoDelAngel <estuardodelangel29@gmail.com>
2022-06-11 00:00:58 +0100

    Removed comment

--- app/Main.hs
+++ app/Main.hs
@@ -12,20 +12,3 @@ main = do
         Just out -> show out
         _ -> "invalid command"
     main
-
-
--- trig exact ratios
--- integer + rational powers
--- like terms
--- terms
--- exact logs
--- maybe convert all negs to -1 then call another function to return them ?
--- turn weird trig into normal trig ? hyperbolics ?
--- 1/(thing)^power
--- collect like terms
--- exact roots
-
--- expand, factor
--- integrate
--- evaluate
--- other stuff idk
